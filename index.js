54e04c1a6241cbc50335ce08a4ad1ea7a04ab237
Britto Thomas <brittovadakkethala@gmail.com>
2019-10-15 09:10:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+var mySuperHero = superheroes.random();
+var mySuperVillain = supervillains.random();
+
+console.log("My Super Hero - " + mySuperHero);
+console.log("My Super Villain - " + mySuperVillain);
